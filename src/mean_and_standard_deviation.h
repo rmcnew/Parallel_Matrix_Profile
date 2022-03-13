5cffb9a94abb7cc16f8e69ec464b7e607a5b7066
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2022-03-12 19:38:12 -0700

    Add distance_profile and mean_and_standard_deviation stubs

---
+++ src/mean_and_standard_deviation.h
@@ -0,0 +1,13 @@
+#ifndef GROUP_010_MEAN_AND_STANDARD_DEVIATION_H
+#define GROUP_010_MEAN_AND_STANDARD_DEVIATION_H
+
+#include "types.h"
+
+void compute_mean_and_standard_deviation(
+        const DoubleArray& query_segment, const DoubleArray& time_series,                // input values
+        double& query_segment_mean, double& query_segment_standard_deviation,            // output values
+        double& time_series_window_mean, double& time_series_window_standard_deviation); // output values
+        
+
+#endif // GROUP_010_MEAN_AND_STANDARD_DEVIATION_H
+
