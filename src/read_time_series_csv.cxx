dac5bdab8f789caab462382f0ee403b5baac9220
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2022-03-12 21:09:34 -0700

    Add csv-parser library

--- src/read_time_series_csv.cxx
+++ src/read_time_series_csv.cxx
@@ -1,12 +1,15 @@
 
+#include "csv.hpp"
 #include "read_time_series_csv.h"
+
 
 
 
 DoubleArray read_time_series_csv_file(std::string input_file, int input_column) {
     DoubleArray time_series;
 
-    // We should use an existing CSV library and wrap it accordingly
+    // csv-parser has been included and is available through "csv.hpp"
+    // See documentation: https://github.com/vincentlaucsb/csv-parser
 
     // open input_file
     // read values from the specified input_column (we assume computer's memory will hold all the values)
