14325ed007e6482001b371570bb4bccedcff34f1
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2022-03-22 19:35:28 -0600

    Update matrix_profile.cxx to remove non-blocking send/receive logic

--- src/matrix_profile.cxx
+++ src/matrix_profile.cxx
@@ -30,70 +30,6 @@ MatrixProfile matrix_profile;
 DoubleArray distance_profile;
 
 
-// Globals for sending and receiving
-struct outbound_message {
-    int data;
-    int destination;
-    int tag;
-};
-
-int inbound_data;
-bool inbound_request_in_progress = false;
-MPI_Request inbound_request;
-
-outbound_message outbound_data;
-bool outbound_request_in_progress = false;
-MPI_Request outbound_request;
-std::queue<outbound_message> outbound_queue;
-
-
-
-
-
-// ***** Message Handling *****
-void receive_inbound_messages() {
-    int data_ready;
-    MPI_Status status;
-
-    do {
-        if (!inbound_request_in_progress) {
-            MPI_Irecv(&inbound_data, ONE_MESSAGE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &inbound_request);
-            inbound_request_in_progress = true;
-            data_ready = FALSE;
-        }
-        if (inbound_request_in_progress) {
-            MPI_Test(&inbound_request, &data_ready, &status);
-        }
-        if (data_ready) {
-            inbound_request_in_progress = false;
-
-            // do something with inbound data
-        }
-    } while (!inbound_request_in_progress);
-}
-
-void send_outbound_messages() {
-    int data_sent = FALSE;
-    MPI_Status status;
-
-    do {
-        if (!outbound_request_in_progress && outbound_queue.size() > 0) {
-            outbound_data = outbound_queue.front();
-            MPI_Isend(&(outbound_data.data), ONE_MESSAGE, MPI_INT, outbound_data.destination, outbound_data.tag, MPI_COMM_WORLD, &outbound_request);
-            outbound_request_in_progress = true;
-            data_sent = FALSE;
-        }
-        if (outbound_request_in_progress) {
-            MPI_Test(&outbound_request, &data_sent, &status);
-        }
-        if (data_sent) {
-            outbound_queue.pop();
-            outbound_request_in_progress = false;
-        }
-    } while (!outbound_request_in_progress && outbound_queue.size() > 0);
-}
-
-
 // ***** Initializations *****
 void initialize_MPI(int argc, char* argv[]) {
     // Initialize MPI
@@ -102,10 +38,8 @@ void initialize_MPI(int argc, char* argv[]) {
     // Get rank and process count
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &process_count);  
-}
 
-void initialize_prng() {
-	srand(static_cast<unsigned>(time(NULL)));
+
 }
 
 
@@ -113,7 +47,6 @@ int main(int argc, char** argv) {
     // put command line args in string vector
     std::vector<std::string> args(argv + 1, argv + argc);
 
-    initialize_prng();
     initialize_MPI(argc, argv);
 
     command_line_args = parse_command_line_args(args);
@@ -121,6 +54,19 @@ int main(int argc, char** argv) {
 
     start_timer();
     //start_logging();
+
+    // MPI_Bcast time_series to non-Leader processes
+
+    // MPI_Bcast window_size to non-Leader processes
+
+    // Each process calculates distance profiles for its portion of the time_series indices 
+    // and updates its own local matrix_profile using element-wise minimum (STAMP)
+
+    // Each non-Leader process MPI_Send's local matrix_profile to Leader
+    // Leader MPI_Recv's matrix_profiles from non-Leader processes and updates
+    // the global matrix_profile using element-wise minimum (see STAMP)
+
+    // Leader process writes out global matrix_profile to output_file
 
     stop_timer();
     //stop_logging();
