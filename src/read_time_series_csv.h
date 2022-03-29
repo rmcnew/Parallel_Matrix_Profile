7b1befe9a97958ae1f60cd8b0b62ecd04c841508
Brody <brody.larsen@hotmail.com>
2022-03-28 19:50:54 -0600

    attempt at reading in times series csv

--- src/read_time_series_csv.h
+++ src/read_time_series_csv.h
@@ -6,7 +6,7 @@
 #include "types.h"
 
 
-DoubleArray read_time_series_csv_file(std::string input_file, int input_column);
+list<list <float>> read_time_series_csv_file(std::string input_file, int input_column);
 
 
 
