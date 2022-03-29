7ac893a28dbe76c17494b327b486aa4ab31c152c
Brody <brody.larsen@hotmail.com>
2022-03-28 19:58:37 -0600

    failed attempt at testing googel tests

--- tests/command_line_args_test.cxx
+++ tests/command_line_args_test.cxx
@@ -1,27 +1,37 @@
 #include <gtest/gtest.h>
 #include "command_line_args.h"
+#include <iostream>
+#include <vector>
 
 // rank and process_count must be defined external to the
 // command_ling_args functions to simulate the MPI variables
 int rank = 0;
 int process_count = 4;
 
+
 TEST(CommandLineArgsTest, test_command_line_args) {
     
-    /* Test for command_line_args:
+    //Test for command_line_args:
 
     void print_usage();
+    
+    // create string vector of simulated command line args
+    std::vector<std::string> args;
+    args.push_back("input.csv");
+    args.push_back("output.csv");
+    args.push_back("10");
+    args.push_back("-5");
 
-    CommandLineArgs parse_command_line_args(std::vector<std::string> args);
+    // call parse_command_line_args
+    CommandLineArgs parsed_args;
+    parsed_args = parse_command_line_args(args);
 
+    // call validate_command_line_args
     void validate_command_line_args(const CommandLineArgs& command_line_args);
 
-    */
-
-    // create string vector of simulated command line args
-    // call parse_command_line_args
+    EXPECT_TRUE(validate_command_line_args);
     // verify returned CommandLineArgs struct for simulated command line arg values
-    // call validate_command_line_args
+    
 
     // Some of the values can cause the process to exit:
     //    -h or --help for parse_command_line_args
@@ -31,6 +41,7 @@ TEST(CommandLineArgsTest, test_command_line_args) {
     // Google Test can check for process exit using special assertions called "Death Assertions"
     // See "Death Assertions" in https://google.github.io/googletest/reference/assertions.html
     
+    
     // create a string vector with --help
     // should print the usage message and exit
 
@@ -38,7 +49,7 @@ TEST(CommandLineArgsTest, test_command_line_args) {
     // should print which value(s) are invalid, print usage message, and exit
     
 
-    EXPECT_TRUE(true);
+    //EXPECT_TRUE(true);
 }
 
 
